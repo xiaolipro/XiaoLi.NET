334bab4d206c623e8970f9a0abb1a81cc60c02d5
Antinew <2357729423@qq.com>
2022-08-01 09:08:29 +0800

    rm

--- src/XiaoLi.EventBus.RabbitMQ.UnitTest/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-namespace XiaoLi.EventBus.RabbitMQ.UnitTest;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
--- src/XiaoLi.EventBus.Tests/Usings.cs
+++
@@ -1,9 +0,0 @@
-global using Xunit;
-
-global using System;
-global using System.Collections.Generic;
-global using System.Linq;
-global using System.Text;
-global using System.Threading.Tasks;
-global using XiaoLi.EventBus.Events;
-global using XiaoLi.EventBus.UnitTests.IntegrationEvents.Events;
