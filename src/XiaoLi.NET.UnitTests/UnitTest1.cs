d47b0f97d697e2f3ac446e54eb93ea92c1d705b9
Antinew <2357729423@qq.com>
2022-08-07 22:43:23 +0800

    测试

--- src/XiaoLi.NET.UnitTests/UnitTest1.cs
+++ src/XiaoLi.NET.UnitTests/UnitTest1.cs
@@ -1,3 +1,6 @@
+using System.Threading.Channels;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+
 namespace XiaoLi.NET.UnitTests
 {
     public class UnitTest1
@@ -5,7 +8,13 @@ namespace XiaoLi.NET.UnitTests
         [Fact]
         public void Test1()
         {
-
+            string str = "sadkhkajsfhjer";
+            for (int i = 0; i < str.Length; i++)
+            {
+                var item = str[i];
+                ConsoleOutput.Instance.Write(item.ToString(),OutputLevel.Information);
+                item = 'a';
+            }
         }
     }
 }
