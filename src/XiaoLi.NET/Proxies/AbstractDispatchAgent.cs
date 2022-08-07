61039adb34c95194d6427d2cc92d50cf253e8317
Antinew <2357729423@qq.com>
2022-08-07 22:43:06 +0800

    代理

---
+++ src/XiaoLi.NET/Proxies/AbstractDispatchAgent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XiaoLi.NET.Proxies
+{
+    public abstract class AbstractDispatchAgent
+    {
+        public static TProxy
+    }
+}
