d5dfe2b27e6bc5634ed401ebef70b133b729cbfc
Antinew <2357729423@qq.com>
2022-08-13 21:11:16 +0800

    loadbalance

---
+++ src/XiaoLi.NET.Grpc/LoadBalancers/CustomBalancerFactory.cs
@@ -0,0 +1,60 @@
+﻿using Grpc.Net.Client.Balancer;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using XiaoLi.NET.LoadBalancers;
+
+namespace XiaoLi.NET.Grpc.LoadBalancers
+{
+    public class CustomBalancerFactory : LoadBalancerFactory
+    {
+        private readonly IGrpcLoadBalancer _grpcLoadBalancer;
+
+        public override string Name => string.IsNullOrWhiteSpace(_grpcLoadBalancer.Name)?"grpc":_grpcLoadBalancer.Name;
+        public CustomBalancerFactory(IGrpcLoadBalancer grpcLoadBalancer)
+        {
+            _grpcLoadBalancer = grpcLoadBalancer;
+        }
+        public override LoadBalancer Create(LoadBalancerOptions options)
+        {
+            return new CustomBalancer(options.Controller, options.LoggerFactory, _grpcLoadBalancer);
+        }
+
+
+        internal class CustomBalancer : SubchannelsLoadBalancer
+        {
+            private readonly IGrpcLoadBalancer _grpcLoadBalancer;
+
+            public CustomBalancer(IChannelControlHelper controller, ILoggerFactory loggerFactory, IGrpcLoadBalancer grpcLoadBalancer)
+                : base(controller, loggerFactory)
+            {
+                _grpcLoadBalancer = grpcLoadBalancer;
+            }
+
+
+            protected override SubchannelPicker CreatePicker(IReadOnlyList<Subchannel> readySubchannels)
+            {
+                return new ConsulPicker(readySubchannels, _grpcLoadBalancer);
+            }
+
+            private class ConsulPicker : SubchannelPicker
+            {
+                private readonly IReadOnlyList<Subchannel> _subchannels;
+                private readonly IGrpcLoadBalancer _grpcLoadBalancer;
+
+                public ConsulPicker(IReadOnlyList<Subchannel> subchannels, IGrpcLoadBalancer grpcLoadBalancer)
+                {
+                    _subchannels = subchannels;
+                    _grpcLoadBalancer = grpcLoadBalancer;
+                }
+
+                public override PickResult Pick(PickContext context)
+                {
+                    int index = _grpcLoadBalancer.GetBalancedIndex(_subchannels.Count);
+                    // Pick a random subchannel.
+                    return PickResult.ForSubchannel(_subchannels[index]);
+                }
+            }
+        }
+
+    }
+}
---
+++ src/XiaoLi.NET.Grpc/LoadBalancers/CustomResolverFactory.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Grpc.Net.Client.Balancer;
+using Microsoft.Extensions.Logging;
+using XiaoLi.NET.LoadBalancers;
+
+namespace XiaoLi.NET.Grpc.LoadBalancers
+{
+    public class CustomResolverFactory : ResolverFactory
+    {
+        private readonly IGrpcLoadBalancer _grpcLoadBalancer;
+
+        // Create a FileResolver when the URI has a 'grpc' scheme.
+        public override string Name => string.IsNullOrWhiteSpace(_grpcLoadBalancer.Name) ? "grpc" : _grpcLoadBalancer.Name;
+
+        public CustomResolverFactory(IGrpcLoadBalancer grpcLoadBalancer)
+        {
+            _grpcLoadBalancer = grpcLoadBalancer;
+        }
+
+        public override Resolver Create(ResolverOptions options)
+        {
+            return new CustomResolver(options.LoggerFactory, _grpcLoadBalancer, options.Address, options.DefaultPort);
+        }
+
+        internal class CustomResolver : PollingResolver
+        {
+            private readonly Uri _address;
+            private readonly int _defaultPort;
+            private readonly IGrpcLoadBalancer _grpcLoadBalancer;
+
+            public CustomResolver(ILoggerFactory loggerFactory, IGrpcLoadBalancer grpcLoadBalancer, Uri address,
+                int defaultPort) : base(loggerFactory)
+            {
+                _grpcLoadBalancer = grpcLoadBalancer;
+                _address = address;
+                _defaultPort = defaultPort;
+            }
+
+
+            protected override async Task ResolveAsync(CancellationToken cancellationToken)
+            {
+                // 获取服务对应的所有主机
+                var uris = await _grpcLoadBalancer.GetServiceUris(_address.Host);
+
+                var addresses = uris.Select(uri => new BalancerAddress(uri.Host, uri.Port)).ToArray();
+
+                // 将结果传递回通道。
+                Listener(ResolverResult.ForResult(addresses));
+            }
+        }
+    }
+}
