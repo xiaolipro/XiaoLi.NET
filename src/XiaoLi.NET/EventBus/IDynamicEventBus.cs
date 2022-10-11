8fe9585078dd775bca0ebd78f7c61bea6cf71158
xiaoli <2357729423@qq.com>
2022-10-11 09:17:31 +0800

    动态事件总线

---
+++ src/XiaoLi.NET/EventBus/IDynamicEventBus.cs
@@ -0,0 +1,26 @@
+﻿using XiaoLi.NET.EventBus.Events;
+
+namespace XiaoLi.NET.EventBus
+{
+    /// <summary>
+    /// 动态事件总线
+    /// </summary>
+    public interface IDynamicEventBus:IEventBus
+    {
+        /// <summary>
+        /// 订阅动态事件
+        /// </summary>
+        /// <typeparam name="THandler">动态事件处理者</typeparam>
+        /// <param name="eventName">动态事件名称</param>
+        void SubscribeDynamic<THandler>(string eventName)
+            where THandler : IDynamicIntegrationEventHandler;
+        
+        /// <summary>
+        /// 解阅动态事件
+        /// </summary>
+        /// <typeparam name="THandler">动态事件处理者</typeparam>
+        /// <param name="eventName">动态事件名称</param>
+        void UnsubscribeDynamic<THandler>(string eventName)
+            where THandler : IDynamicIntegrationEventHandler;
+    }
+}
--- src/XiaoLi.NET/EventBus/IEventBus.cs
+++ src/XiaoLi.NET/EventBus/IEventBus.cs
@@ -8,7 +8,6 @@ namespace XiaoLi.NET.EventBus
     /// <summary>
     /// 事件总线
     /// </summary>
-    /// <remarks>支持发布，动态订阅、解阅，支持动态集成事件</remarks>
     public interface IEventBus
     {
         /// <summary>
@@ -27,14 +26,6 @@ namespace XiaoLi.NET.EventBus
             where THandler : IIntegrationEventHandler<TEvent>;
 
         /// <summary>
-        /// 订阅动态事件
-        /// </summary>
-        /// <typeparam name="THandler">动态事件处理者</typeparam>
-        /// <param name="eventName">动态事件名称</param>
-        void SubscribeDynamic<THandler>(string eventName)
-            where THandler : IDynamicIntegrationEventHandler;
-
-        /// <summary>
         /// 解阅事件
         /// </summary>
         /// <typeparam name="TEvent">事件</typeparam>
@@ -43,12 +34,5 @@ namespace XiaoLi.NET.EventBus
             where TEvent : IntegrationEvent
             where THandler : IIntegrationEventHandler<TEvent>;
 
-        /// <summary>
-        /// 解阅动态事件
-        /// </summary>
-        /// <typeparam name="THandler">动态事件处理者</typeparam>
-        /// <param name="eventName">动态事件名称</param>
-        void UnsubscribeDynamic<THandler>(string eventName)
-            where THandler : IDynamicIntegrationEventHandler;
     }
 }
