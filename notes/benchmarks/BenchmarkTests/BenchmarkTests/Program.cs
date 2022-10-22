d37c04875068e09473641b3705a477021b460e0a
xiaoli <2357729423@qq.com>
2022-10-22 14:41:49 +0800

    pre

--- notes/benchmarks/BenchmarkTests/BenchmarkTests/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using BenchmarkDotNet.Running;
-using BenchmarkTests;
-
-BenchmarkRunner.Run<ChannelBenchmark>();
-
-Console.ReadKey();
--- notes/multiThreading/3.SynchronizationBase/ThreadingTests/EventWaitHandles/WaitPulseTests.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ThreadingTests.EventWaitHandles;
-
-public class WaitPulseTests
-{
-    [Fact]
-    void 生产消费模型测试()
-    {
-    }
-}
