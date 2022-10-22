c1c5b2185c4fe3844bbe17603b1a37a685797553
Antinew <2357729423@qq.com>
2022-10-22 11:59:24 +0800

    benchmarks

---
+++ notes/benchmarks/BenchmarkTests/BenchmarkTests/ChannelBenchmark.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Concurrent;
+using System.Threading.Channels;
+using BenchmarkDotNet.Attributes;
+
+namespace BenchmarkTests;
+
+[MemoryDiagnoser]
+public class ChannelBenchmark
+{
+    private readonly Channel<string> _channel;
+    private readonly ConcurrentQueue<string> _queue;
+
+    public ChannelBenchmark()
+    {
+        _channel = Channel.CreateUnbounded<string>();
+        _queue = new ConcurrentQueue<string>();
+    }
+
+    [Benchmark]
+    public async Task ChannelRWAsync()
+    {
+        await _channel.Writer.WriteAsync("hello");
+        var _ = await _channel.Reader.ReadAsync();
+    }
+    
+    [Benchmark]
+    public void ChannelRW()
+    {
+        _channel.Writer.TryWrite("hello");
+        _channel.Reader.TryRead(out _);
+    }
+    
+    [Benchmark]
+    public void ConcurrentQueueRW()
+    {
+        _queue.Enqueue("hello");
+        _queue.TryDequeue(out _);
+    }
+}
---
+++ notes/benchmarks/BenchmarkTests/BenchmarkTests/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Running;
+using BenchmarkTests;
+
+BenchmarkRunner.Run<ChannelBenchmark>();
+
+Console.ReadKey();
