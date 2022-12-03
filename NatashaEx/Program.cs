e490125f5a1d9be8cc6691f3eaafb92d393d693e
李鹏军 <2357729423@qq.com>
2022-12-03 18:06:56 +0800

    1

--- NatashaEx/Program.cs
+++ NatashaEx/Program.cs
@@ -7,8 +7,8 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 NatashaInitializer.Preheating();
 string script = """   
-int _customerCode = int.Parse(arg[0]);
-string _appKey = arg[1];
+int _customerCode = int.Parse(arg.customerCode);
+string _appKey = arg.appKey;
 long _timestamp = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds();
 string pwd = _customerCode + _appKey + _timestamp;
 
@@ -34,23 +34,25 @@ string MD5Encrypt32(string password = "")
 return new {
     timeStamp = _timestamp.ToString(),
     sign = MD5Encrypt32(pwd),
-    customerCode = _customerCode.ToString()
+    customerCode = _customerCode.ToString(),
+    body = arg2
 };
 """;
 
-script = "public static object A() { int _customerCode=int.Parse(arg[0]);string _appKey=arg[1];long _timestamp=new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds();string pwd=_customerCode+_appKey+_timestamp;string MD5Encrypt32(string password=\"\") {     string empty = string.Empty;     try     {         if (!string.IsNullOrEmpty(password) && !string.IsNullOrWhiteSpace(password))         {             foreach (byte num in MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(password)))                 empty += num.ToString(\"X2\");         }     }     catch     {         throw new Exception(\"错误的 password 字符串:【\" + password + \"】\");     }      return empty; }  return new {     timeStamp = _timestamp.ToString(),     sign = MD5Encrypt32(pwd),     customerCode = _customerCode.ToString() };}";
+//script = "public static object A() { int _customerCode=int.Parse(arg[0]);string _appKey=arg[1];long _timestamp=new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds();string pwd=_customerCode+_appKey+_timestamp;string MD5Encrypt32(string password=\"\") {     string empty = string.Empty;     try     {         if (!string.IsNullOrEmpty(password) && !string.IsNullOrWhiteSpace(password))         {             foreach (byte num in MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(password)))                 empty += num.ToString(\"X2\");         }     }     catch     {         throw new Exception(\"错误的 password 字符串:【\" + password + \"】\");     }      return empty; }  return new {     timeStamp = _timestamp.ToString(),     sign = MD5Encrypt32(pwd),     customerCode = _customerCode.ToString() };}";
  var memberDeclaration = CSharpSyntaxTree.ParseText(script)
      .GetRoot()
      .DescendantNodes()
      .FirstOrDefault();
 Console.WriteLine(memberDeclaration.NormalizeWhitespace().ToFullString());
 var action = NDelegate.RandomDomain()
-        .Func<string[],object>(script);
-    var res = action(new string[]
-    {
-        "1255",
-        "a1255yunlian20220426PEYGDJEY548654NCSOSHS"
-    });
+        .Func<dynamic,object>(script);
+dynamic t = new
+{
+    customerCode="1255",
+    appKey="a1255yunlian20220426PEYGDJEY548654NCSOSHS"
+};
+    var res = action(t);
     
     foreach (var item in res.GetType().GetProperties())
     {
